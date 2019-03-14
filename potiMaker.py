ff74b8bb5c81dcadaee4411d5d620cef4a2d96fa
Santiago Parladé Adeva <santiparlade97@gmail.com>
2019-03-14 21:11:14 +0000

    Add files via upload

--- potiMaker.py
+++ potiMaker.py
@@ -1,9 +1,7 @@
 from PIL import Image
 from natsort import natsorted
+from pdf2image import convert_from_path
 import os
-import string
-import random
-import shutil
 
 class Pecha(object):
 	def __init__(self, inputFormat, inputLocation, outputName, outputLocation, outputSize):
@@ -24,9 +22,13 @@ class Pecha(object):
 		self.optimalWidth = 0
 		self.optimalHeight = 0
 		self.optimalHeightTotal = 0
-		self.tempFolder = ""
 	
 	def potiMaker(self):
+		if self.outputSize == "A4":
+			self.optimalWidth, self.optimalHeight, self.optimalHeightTotal = (3508, 827, 2481)
+		elif self.outputSize == "A3":
+			self.optimalWidth, self.optimalHeight, self.optimalHeightTotal = (4961, 1168, 3508)
+
 		self.collectFiles()
 		self.resizeImages()
 		self.orderImages()
@@ -34,71 +36,52 @@ class Pecha(object):
 		print("Done! ;)")
 
 	def collectFiles(self):
-		self.tempFolder = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
-		if os.path.isdir("./%s/" % self.tempFolder):
-			self.tempFolder = ''.join(random.choices(string.ascii_uppercase + string.digits, k=15))
-		os.mkdir("./%s/" % self.tempFolder)
-
 		if self.inputFormat == "pdf":
-			file = open(self.inputLocation, "rb")
-			pdf = file.read()
-
-			startmark = b"\xff\xd8"
-			startfix = 0
-			endmark = b"\xff\xd9"
-			endfix = 2
-			i = 0
-
-			njpg = 1
-			while True:
-				istream = pdf.find(b"stream", i)
-				if istream < 0:
-					break
-
-				istart = pdf.find(startmark, istream, istream+20)
-				if istart < 0:
-					i = istream+20
-					continue
-
-				iend = pdf.find(b"endstream", istart)
-				if iend < 0:
-					raise Exception("Didn't find end of stream!")
-
-				iend = pdf.find(endmark, iend-20)
-				if iend < 0:
-					raise Exception("Didn't find end of JPG!")
-
-				istart += startfix
-				iend += endfix
-				jpg = pdf[istart:iend]
-
-				if njpg < 10:
-					num = 0
-					jpgfile = open("./%s/jpg%d%d.jpg" % (self.tempFolder, num, njpg), "wb")
-				else:
-					jpgfile = open("./%s/jpg%d.jpg" % (self.tempFolder, njpg), "wb")
-					pass
-
-				jpgfile.write(jpg)
-				jpgfile.close()
-
-				njpg += 1
-				i = iend
+#			file = open(self.inputLocation, "rb")
+#			pdf = file.read()
+#
+#			startmark = b"\xff\xd8"
+#			startfix = 0
+#			endmark = b"\xff\xd9"
+#			endfix = 2
+#			i = 0
+#
+#			njpg = 1
+#			while True:
+#				istream = pdf.find(b"stream", i)
+#				if istream < 0:
+#					break
+#
+#				istart = pdf.find(startmark, istream, istream+20)
+#				if istart < 0:
+#					i = istream+20
+#					continue
+#
+#				iend = pdf.find(b"endstream", istart)
+#				if iend < 0:
+#					raise Exception("Didn't find end of stream!")
+#
+#				iend = pdf.find(endmark, iend-20)
+#				if iend < 0:
+#					raise Exception("Didn't find end of JPG!")
+#
+#				istart += startfix
+#				iend += endfix
+#				jpg = pdf[istart:iend]
+#
+#				self.jpgImages.append(Image.frombytes('RGB', (self.optimalWidth,self.optimalHeight), jpg))
+#
+#				njpg += 1
+#				i = iend
+			self.jpgImages = convert_from_path(self.inputLocation, 300)
 
 		elif self.inputFormat == "jpg":
 			inputJpgs = [file for file in natsorted(os.listdir(self.inputLocation)) if file.endswith(".jpg")]
 			for i in range(len(inputJpgs)):
 				currentImg = Image.open(self.inputLocation+inputJpgs[i])
-				if i < 9:
-					num = 0
-					currentImg.save('./%s/jpg%d%d.jpg' % (self.tempFolder, num, (i+1)))
-				else:
-					currentImg.save('./%s/jpg%d.jpg' % (self.tempFolder, (i+1)))
-					pass
-				pass
+				self.jpgImages.append(currentImg)
 			pass
 
-		self.jpgImages = [file for file in os.listdir('./%s/' % self.tempFolder) if file.endswith(".jpg")]
 		self.totalImages = len(self.jpgImages)
 		self.totalPages = self.totalImages // 3
 		self.difference = self.totalImages % 3
@@ -107,13 +90,8 @@ class Pecha(object):
 			pass
 
 	def resizeImages(self):
-		if self.outputSize == "A4":
-			self.optimalWidth, self.optimalHeight, self.optimalHeightTotal = (3508, 827, 2481)
-		elif self.outputSize == "A3":
-			self.optimalWidth, self.optimalHeight, self.optimalHeightTotal = (4961, 1168, 3508)
-
 		for i in range(0,self.totalImages,1):
-			currentImg = Image.open('./%s/'% self.tempFolder+self.jpgImages[i])
+			currentImg = self.jpgImages[i]
 			width, height = currentImg.size
 			currentAspectRatio = width / height
 			if self.aspectRatio >= currentAspectRatio:
@@ -176,8 +154,6 @@ class Pecha(object):
 		self.outputName = self.outputName+".pdf"
 		self.finalFiles[0].save(self.outputLocation+self.outputName, save_all=True, append_images=self.finalFiles[1:])
 
-		shutil.rmtree('./%s/' % self.tempFolder)
-
 
 
 
@@ -185,7 +161,7 @@ class Pecha(object):
 
 #inputFormat, inputLocation, ouputName, outputLocation, outputSize
 
-poti = Pecha("pdf", "./inputFiles/test.pdf", "out", "./", "A4")
-#poti = Pecha("jpg", "./inputFiles/", "out", "./", "A4")
+#poti = Pecha("pdf", "./inputFiles/test.pdf", "out", "./", "A4")
+poti = Pecha("jpg", "./inputFiles/", "out", "./", "A4")
 
 poti.potiMaker()
